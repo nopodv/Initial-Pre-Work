9b29a332884fe11c173e380a0bb19febf609f170
nopoabb <huntergreen_2k@yahoo.com>
2019-05-19 14:30:34 -0400

    Added string variable with double quotes and comments about what I Iearned.

---
+++ 5-Introduction-to-JavaScript/22-Escaping-Literal-Quotes-in-Strings.js
@@ -0,0 +1,5 @@
+// Escape literal quotes in strings
+
+/* Including an actual quote that someone said can cause javascript to think
+the string is finished, but this is a way to get around that issue. */
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
