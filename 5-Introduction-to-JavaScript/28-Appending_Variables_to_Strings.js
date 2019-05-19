69abd2a9e189df904413f9b18cd17c8c300e5a4d
nopoabb <huntergreen_2k@yahoo.com>
2019-05-19 15:30:14 -0400

    Added variable to string using plus equals and comments about what I Iearned.

---
+++ 5-Introduction-to-JavaScript/28-Appending_Variables_to_Strings.js
@@ -0,0 +1,13 @@
+// Appending variables to strings
+
+/* Appends new variable to existing string variable with the plus equals operator. */
+// Example
+var anAdjective = "awesome!";
+var ourStr = "freeCodeCamp is ";
+ourStr += anAdjective;
+
+// Only change code below this line
+
+var someAdjective = "mind boggling!";
+var myStr = "Learning to code is ";
+myStr += someAdjective;
