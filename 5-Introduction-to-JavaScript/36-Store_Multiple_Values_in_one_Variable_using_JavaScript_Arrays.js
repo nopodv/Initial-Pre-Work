3a42a8a1ee6b7ed82b303913faaece5444cea716
nopoabb <huntergreen_2k@yahoo.com>
2019-05-19 19:12:38 -0400

    Added array and comments about what I Iearned.

---
+++ 5-Introduction-to-JavaScript/36-Store_Multiple_Values_in_one_Variable_using_JavaScript_Arrays.js
@@ -0,0 +1,8 @@
+// Store multiple values in one variable with arrays
+
+// Will combine many items into one variable. A real space saver.
+// Example
+var ourArray = ["John", 23];
+
+// Only change code below this line.
+var myArray = ["soup", 50];
