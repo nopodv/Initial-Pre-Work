8c13aa13153304d13ca5d98048d9cf7f28927580
nopoabb <huntergreen_2k@yahoo.com>
2019-05-19 15:14:09 -0400

    Added strings with plus equals and comments about what I Iearned.

---
+++ 5-Introduction-to-JavaScript/26-Concatenating_Strings_with_the_Plus_Equals_Operator.js
@@ -0,0 +1,12 @@
+// Concatenate with plus equals operator
+
+/* Be sure to remember to add spacing to strings when adding
+the plus equals operator. */
+// Example
+var ourStr = "I come first. ";
+ourStr += "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
