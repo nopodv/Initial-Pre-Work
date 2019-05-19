df66854c79f23c2e86dff654b7ecbe95d94fb940
nopoabb <huntergreen_2k@yahoo.com>
2019-05-19 19:16:59 -0400

    Added nested array and comments about what I Iearned.

---
+++ 5-Introduction-to-JavaScript/37-Nest_one_Array_within_Another_Array.js
@@ -0,0 +1,8 @@
+// Nest one array within another
+
+// Groups two or more arrays into another array...therefore it's nested.
+// Example
+var ourArray = [["the universe", 42], ["everything", 101010]];
+
+// Only change code below this line.
+var myArray = [["vacuum", 2],["chairs", 6]];
