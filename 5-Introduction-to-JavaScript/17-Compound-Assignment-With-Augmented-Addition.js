99053f40bf9eeeb981a7bdb42584f2b9e2958463
nopoabb <huntergreen_2k@yahoo.com>
2019-05-19 13:56:04 -0400

    Added compound assigment and comments about what I Iearned.

---
+++ 5-Introduction-to-JavaScript/17-Compound-Assignment-With-Augmented-Addition.js
@@ -0,0 +1,13 @@
+// Compound assignment with augmented addition
+
+/* Does 2 things at once - doing math and assigning the variable in one step
+can save time. */
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Only modify code below this line
+
+a += 12;
+b += 9;
+c += 7;
