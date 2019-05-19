d3863288990147ca0503503619bc4d6fa1c31e6b
nopoabb <huntergreen_2k@yahoo.com>
2019-05-19 12:19:28 -0400

    Added variable, assigns and comments about what I Iearned.

---
+++ 5-Introduction-to-JavaScript/3-Storing-Values-with-the-Assignment-Operator.js
@@ -0,0 +1,11 @@
+// Store values with the assigmnent operator
+
+/* Assigmnent gives a value to the variable and then reassigns the solved
+variable to another variable. */
+// Setup
+var a;
+var b = 2;
+
+// Only change code below this line
+a = 7;
+b = a;
