a8450d2c1e5a8ab4ad169c5ffdffc71e0c48cec5
nopoabb <huntergreen_2k@yahoo.com>
2019-05-19 19:59:11 -0400

    Added array shift and comments about what I Iearned.

---
+++ 5-Introduction-to-JavaScript/43-Manipulate_Arrays_With_shift().js
@@ -0,0 +1,14 @@
+// Manipulate arrays with shift
+
+/* Shift, like pop, will remove an item from an array. Only this time, the
+first item in the array will be taken off. */
+// Example
+var ourArray = ["Stimpson", "J", ["cat"]];
+var removedFromOurArray = ourArray.shift();
+// removedFromOurArray now equals "Stimpson" and ourArray now equals ["J", ["cat"]].
+
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+
+// Only change code below this line.
+var removedFromMyArray = myArray.shift();
