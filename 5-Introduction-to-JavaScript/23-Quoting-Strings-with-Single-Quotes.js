d5b7d9e8878278a2b9f4b2f09ddaf86441a95847
nopoabb <huntergreen_2k@yahoo.com>
2019-05-19 14:41:37 -0400

    Added quotes to remove slashes in string and comments about what I Iearned.

---
+++ 5-Introduction-to-JavaScript/23-Quoting-Strings-with-Single-Quotes.js
@@ -0,0 +1,8 @@
+// Quote strings with single quotes
+
+/* Mixing 2 types of quotes is possible as long as they match - so an opening
+single quote must end with a single quote, starts double - ends double. This
+can be useful in various scenarios.
+The direction of a slash, if used, is important. Knowing when to use it can be
+helpful to ensure cleaner code (i.e. it's easier to read). */
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
