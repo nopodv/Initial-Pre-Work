febab316fa59e5a7f6b32874cfe98193541a7220
nopoabb <huntergreen_2k@yahoo.com>
2019-05-19 12:31:28 -0400

    Added variable, assignments and ensures defined and comments about what I Iearned.

---
+++ 5-Introduction-to-JavaScript/5-Understanding-Uninitialized-Variables.js
@@ -0,0 +1,13 @@
+// Undertand uninitiallized variables
+
+// Declares 3 variables and values are added to ensure there are none undefined.
+// Initialize these three variables
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+// Do not change code below this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
