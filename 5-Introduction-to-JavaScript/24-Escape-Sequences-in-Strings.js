af91318d0ca776a700471f1bdf93d13d37f594d7
nopoabb <huntergreen_2k@yahoo.com>
2019-05-19 14:57:28 -0400

    Added strings with escape sequences and comments about what I Iearned.

---
+++ 5-Introduction-to-JavaScript/24-Escape-Sequences-in-Strings.js
@@ -0,0 +1,5 @@
+// Escape sequences in strings
+
+/* Using these escape sequences can get messy when you have multiple items
+to add to the assigned string such as multiple new lines and slashes. */
+var myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
