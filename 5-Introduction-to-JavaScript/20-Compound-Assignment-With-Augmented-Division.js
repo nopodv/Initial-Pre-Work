b5dcbb6ebda6ba3cb90de55296a10ccf1a3f28f0
nopoabb <huntergreen_2k@yahoo.com>
2019-05-19 14:15:19 -0400

    Added compound assigment with division.

---
+++ 5-Introduction-to-JavaScript/20-Compound-Assignment-With-Augmented-Division.js
@@ -0,0 +1,12 @@
+// Compound assignment with augmented division
+
+// Multiplies variables by a number.
+var a = 48;
+var b = 108;
+var c = 33;
+
+// Only modify code below this line
+
+a /= 12;
+b /= 4;
+c /= 11;
