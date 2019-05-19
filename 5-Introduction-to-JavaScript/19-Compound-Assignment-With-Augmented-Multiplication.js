4d3893f8f297f172a3014bc41edab1063c56e527
nopoabb <huntergreen_2k@yahoo.com>
2019-05-19 14:05:36 -0400

    Added compound assigment with multiplication and comments about what I Iearned.

---
+++ 5-Introduction-to-JavaScript/19-Compound-Assignment-With-Augmented-Multiplication.js
@@ -0,0 +1,12 @@
+// Compound assignment with augmented multiplication
+
+// Multiplies and adds number to value all in one step.
+var a = 5;
+var b = 12;
+var c = 4.6;
+
+// Only modify code below this line
+
+a *= 5;
+b *= 3;
+c *= 10;
