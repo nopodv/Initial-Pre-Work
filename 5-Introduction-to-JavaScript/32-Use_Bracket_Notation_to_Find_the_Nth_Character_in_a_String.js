90e85bc3ec6d4ca0fc4cbb8f4593a2d02512d590
nopoabb <huntergreen_2k@yahoo.com>
2019-05-19 15:56:34 -0400

    Added bracket notation for string and comments about what I Iearned.

---
+++ 5-Introduction-to-JavaScript/32-Use_Bracket_Notation_to_Find_the_Nth_Character_in_a_String.js
@@ -0,0 +1,12 @@
+// Added bracket notation to find the nth character
+
+// Uses bracket notation to find another character in a string.
+// Example
+var firstName = "Ada";
+var secondLetterOfFirstName = firstName[1];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var thirdLetterOfLastName = lastName[2];
