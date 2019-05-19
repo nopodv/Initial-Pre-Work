0e1fdd2029072adf0d2a42a71d9071b5d1dc2354
nopoabb <huntergreen_2k@yahoo.com>
2019-05-19 19:47:18 -0400

    Added pushed array to array and comments about what I Iearned.

---
+++ 5-Introduction-to-JavaScript/41-Manipulate_Arrays_With_push().js
@@ -0,0 +1,14 @@
+// Manipulate array with push
+
+/* Adds another array to the existing array by adding it to the back as the
+last one. */
+// Example
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.push(["happy", "joy"]);
+// ourArray now equals ["Stimpson", "J", "cat", ["happy", "joy"]]
+
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line.
+myArray.push(["dog", 3]);
