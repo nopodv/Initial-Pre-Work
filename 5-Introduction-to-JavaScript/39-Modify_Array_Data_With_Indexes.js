d23cf430fc5993f7d890b3b297719a6d3af4c19f
nopoabb <huntergreen_2k@yahoo.com>
2019-05-19 19:29:25 -0400

    Changed array's variable and comments about what I Iearned.

---
+++ 5-Introduction-to-JavaScript/39-Modify_Array_Data_With_Indexes.js
@@ -0,0 +1,13 @@
+// Modify array data with index
+
+/* To change an item in an array, start by calling the index, then specifying
+what the new item should be, after the equals. */
+// Example
+var ourArray = [18,64,99];
+ourArray[1] = 45; // ourArray now equals [18,45,99].
+
+// Setup
+var myArray = [18,64,99];
+
+// Only change code below this line.
+myArray[0] = 45;
