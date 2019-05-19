ed6797015d8d8387cbd9120c7bf4b4a0595ddc92
nopoabb <huntergreen_2k@yahoo.com>
2019-05-19 15:22:19 -0400

    Added variable: using variable, strings and plus; and comments about what I Iearned.

---
+++ 5-Introduction-to-JavaScript/27-Constructing_Strings_with_Variables.js
@@ -0,0 +1,10 @@
+// Construct strings with variables
+
+/* Placing variables in strings is possible using the plus operator. */
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "Noel Ketchum";
+var myStr = "My name is " + myName + " and I am well!";
