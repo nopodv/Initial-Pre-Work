669ef48a2c053a614257ff673d80a7a76d1b9545
nopoabb <huntergreen_2k@yahoo.com>
2019-05-19 14:00:19 -0400

    Added subtraction compound assigment and comments about what I Iearned.

---
+++ 5-Introduction-to-JavaScript/18-Compound-Assignment-With-Augmented-Subtraction.js
@@ -0,0 +1,13 @@
+// Uses compund assignment with augmented subtraction
+
+/* Subtracts numbers and assigns value to a variable without the seperate
+process of each step individually. */
+var a = 11;
+var b = 9;
+var c = 3;
+
+// Only modify code below this line
+
+a -= 6;
+b -= 15;
+c -= 1;
