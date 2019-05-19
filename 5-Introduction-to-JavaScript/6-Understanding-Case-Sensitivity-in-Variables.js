be89d70b905ec7057aa8d860a68600192ed2b774
nopoabb <huntergreen_2k@yahoo.com>
2019-05-19 12:41:22 -0400

    Added correct camel case and comments about what I Iearned.

---
+++ 5-Introduction-to-JavaScript/6-Understanding-Case-Sensitivity-in-Variables.js
@@ -0,0 +1,14 @@
+// Understand case sensitivity in variables
+
+/* It is recommended to avoid similarly spelled variables, even with upper
+and lower cases different between the two. Also "camelCase" is recommeded for
+variable with 2 or more words as it is proper form. */
+// Declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
