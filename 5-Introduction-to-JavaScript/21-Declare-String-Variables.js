00061ce4963f38097b6af8cc29e57814a9cc2ddd
nopoabb <huntergreen_2k@yahoo.com>
2019-05-19 14:20:19 -0400

    Added name variables from strings and comments about what I Iearned.

---
+++ 5-Introduction-to-JavaScript/21-Declare-String-Variables.js
@@ -0,0 +1,12 @@
+// Declare string variables
+
+/* Assigns a line of characters equal to a varaible. Assigned variables from my
+name. */
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+
+// Only change code below this line
+
+var myFirstName = "Noel";
+var myLastName = "Ketchum";
