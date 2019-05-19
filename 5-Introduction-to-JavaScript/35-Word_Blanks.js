2edb372e2bd7fb0c34df37b1c144afea0bd2bde9
nopoabb <huntergreen_2k@yahoo.com>
2019-05-19 19:04:18 -0400

    Added word blanks and combined for string and comments about what I Iearned.

---
+++ 5-Introduction-to-JavaScript/35-Word_Blanks.js
@@ -0,0 +1,14 @@
+// Word blanks
+
+// Filling in blanks and concatenating the string using words like a mad lib.
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  // Your code below this line
+  var result;
+  result = "Your + "+myAdjective+" + "+myNoun+" + "+myVerb+" + up the hill + "+myAdverb+" + chasing the car.";
+
+  // Your code above this line
+  return result;
+}
+
+// Change the words here to test your function
+wordBlanks("dog", "big", "ran", "quickly");
