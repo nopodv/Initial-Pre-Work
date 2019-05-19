c311ca71a34edd1ab5aa367fa40c4066ecefcea2
nopoabb <huntergreen_2k@yahoo.com>
2019-05-19 19:23:38 -0400

    Added variable and got array data and comments about what I Iearned.

---
+++ 5-Introduction-to-JavaScript/38-Access_Array_Data_with_Indexes.js
@@ -0,0 +1,13 @@
+// Access array data with indexes
+
+/* Use index to get an item number within array. Unlike an index for a variable
+the index for an array finds the item, not a single character. */
+// Example
+var ourArray = [50,60,70];
+var ourData = ourArray[0]; // equals 50
+
+// Setup
+var myArray = [50,60,70];
+
+// Only change code below this line.
+var myData = myArray[0];
