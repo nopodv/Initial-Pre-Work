84efc7ea410675c49629357874a8140a9e51d062
nopoabb <huntergreen_2k@yahoo.com>
2019-05-19 19:42:02 -0400

    Added new variable with nested array's index and comments about what I Iearned.

---
+++ 5-Introduction-to-JavaScript/40-Access_Multi-Dimensional_Arrays_With_Indexes.js
@@ -0,0 +1,9 @@
+// Access multi dimensional arrays with index
+
+/* Accessing arrays must be done in order. The order is outside, then next
+innner, then nezt inner and so on - working from outside, inwards. */
+// Setup
+var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
+
+// Only change code below this line.
+var myData = myArray[2][1];
