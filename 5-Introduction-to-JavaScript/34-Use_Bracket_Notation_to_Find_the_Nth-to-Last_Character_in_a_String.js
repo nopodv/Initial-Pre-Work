6c5b0754568f0c56e16a73c0ddc179458fd4bf95
nopoabb <huntergreen_2k@yahoo.com>
2019-05-19 16:10:17 -0400

    Added bracket notation to find a letter before the end and comments about what I Iearned.

---
+++ 5-Introduction-to-JavaScript/34-Use_Bracket_Notation_to_Find_the_Nth-to-Last_Character_in_a_String.js
@@ -0,0 +1,13 @@
+// Use bracket notation to find the nth to last character in a string
+
+/* Finds a character that is a specific character before the end of the string.
+This uses the index with minus. */
+// Example
+var firstName = "Ada";
+var thirdToLastLetterOfFirstName = firstName[firstName.length - 3];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var secondToLastLetterOfLastName = lastName[lastName.length - 2];
