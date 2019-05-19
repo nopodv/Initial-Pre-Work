4f0d14e3b2ff81d681fd68620390b58319df6c0f
nopoabb <huntergreen_2k@yahoo.com>
2019-05-19 15:05:01 -0400

    Added strings with concatenation and comments about what I Iearned.

---
+++ 5-Introduction-to-JavaScript/25-Concatenating_Strings_with_Plus_Operator.js
@@ -0,0 +1,11 @@
+// Concatenate strings with plus operator
+
+/* Adds 2 strings together with the addition operator. The concatenation will
+not add spaces, so if two strings need to be added, adding an additional
+space is necessary for readability. */
+// Example
+var ourStr = "I come first. " + "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the start." + " This is the end.";
