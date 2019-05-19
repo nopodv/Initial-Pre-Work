bd786c73661a980976653f29beda34d55934e9fd
nopoabb <huntergreen_2k@yahoo.com>
2019-05-19 15:53:35 -0400

    Added correction for string and comments about what I Iearned.

---
+++ 5-Introduction-to-JavaScript/31-Understand_String_Immutability.js
@@ -0,0 +1,10 @@
+// Understand string immutability
+
+/* Strings cannot change. They however can be replaced by placing other values
+in place of the existing values. */
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+
+myStr = "Hello World"; // Fix Me
