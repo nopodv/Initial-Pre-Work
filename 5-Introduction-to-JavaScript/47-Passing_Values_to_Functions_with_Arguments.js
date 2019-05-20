6ccb5f02c9a3fa879f79ab698adeaa36f6ae171c
nopoabb <huntergreen_2k@yahoo.com>
2019-05-19 20:50:38 -0400

    Added values to functions and comments about what I Iearned.

---
+++ 5-Introduction-to-JavaScript/47-Passing_Values_to_Functions_with_Arguments.js
@@ -0,0 +1,16 @@
+// Passing values to functions with arguments
+
+/* Parameters are added to functions when they are defined. Those parameters
+are then assigned values when the function is called. They are called
+arguments at that point. */
+// Example
+function ourFunctionWithArgs(a, b) {
+  console.log(a - b);
+}
+ourFunctionWithArgs(10, 5); // Outputs 5
+
+// Only change code below this line.
+function functionWithArgs(a, b) {
+  console.log(a + b);
+}
+functionWithArgs(10, 50);
