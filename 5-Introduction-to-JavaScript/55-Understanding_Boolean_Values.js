12418fe236abfe4dd2f944df8b77f27ee23c9283
nopoabb <huntergreen_2k@yahoo.com>
2019-05-20 14:44:27 -0400

    Changed boolean and comments about what I Iearned.

---
+++ 5-Introduction-to-JavaScript/55-Understanding_Boolean_Values.js
@@ -0,0 +1,11 @@
+// Understand boolean values
+
+// Booleans are either on or off and do not exist as strings.
+function welcomeToBooleans() {
+
+// Only change code below this line.
+
+return true; // Change this line
+
+// Only change code above this line.
+}
