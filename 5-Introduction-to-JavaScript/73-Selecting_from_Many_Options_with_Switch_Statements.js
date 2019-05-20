4294a1d6f9d8b813a951ffa50c278720fd67cfc2
nopoabb <huntergreen_2k@yahoo.com>
2019-05-20 18:21:29 -0400

    Added switch statements and comments about what I Iearned.

---
+++ 5-Introduction-to-JavaScript/73-Selecting_from_Many_Options_with_Switch_Statements.js
@@ -0,0 +1,26 @@
+// Selecting from many options with switch statements
+
+// Gives various depending on the values that are changed.
+function caseInSwitch(val) {
+  var answer = "";
+  // Only change code below this line
+    switch(val) {
+    case 1:
+      return "alpha";
+      break;
+    case 2:
+      return "beta";
+      break;
+    case 3:
+      return "gamma";
+      break;
+    case 4:
+      return "delta";
+      break;
+  }
+  // Only change code above this line
+  return answer;  
+}
+
+// Change this value to test
+caseInSwitch(1);
