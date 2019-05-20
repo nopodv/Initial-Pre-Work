73282bfeb781bbe3893312f80c9ccfc59d3b88c1
nopoabb <huntergreen_2k@yahoo.com>
2019-05-20 16:20:11 -0400

    Added logical and operator and 2 operands and comments about what I Iearned.

---
+++ 5-Introduction-to-JavaScript/66-Comparisons_with_the_Logical_And_Operator.js
@@ -0,0 +1,16 @@
+// Compare the logical and operator
+
+// Two statements test with 2 operands and the logical and operator.
+function testLogicalAnd(val) {
+  // Only change code below this line
+
+  if (val <= 50 && val >= 25) {
+      return "Yes";
+  }
+
+  // Only change code above this line
+  return "No";
+}
+
+// Change this value to test
+testLogicalAnd(10);
