ad05afb62562bace07d0bfba26060021e83b03ba
nopoabb <huntergreen_2k@yahoo.com>
2019-05-19 21:56:21 -0400

    Added function and comments about what I Iearned.

---
+++ 5-Introduction-to-JavaScript/52-Understanding_Undefined_Value_returned_from_a_Function.js
@@ -0,0 +1,17 @@
+// Understand undefined value returned from a function
+
+// Adds function which returns undefined value.
+// Example
+var sum = 0;
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+function addFive(num) {
+ sum = sum + 5;
+}
+
+
+// Only change code above this line
+var returnedValue = addFive();
