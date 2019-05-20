89f5234e5f7a3acc44c6864618cefe28b9c8a17a
nopoabb <huntergreen_2k@yahoo.com>
2019-05-20 15:22:20 -0400

    Added strict equal operator and comments about what I Iearned.

---
+++ 5-Introduction-to-JavaScript/59-Practice_comparing_different_values.js
@@ -0,0 +1,13 @@
+// Practice comparing different values
+
+// Sets strictly equal to allow only the correct type and equal values.
+// Setup
+function compareEquality(a, b) {
+  if (a === b) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+compareEquality(10, 10);
