599e9e93d848f47fce1e59aa356c8cdf932563fb
nopoabb <huntergreen_2k@yahoo.com>
2019-05-19 21:09:18 -0400

    Added var and values to variables and comments about what I Iearned.

---
+++ 5-Introduction-to-JavaScript/48-Global_Scope_and_Functions.js
@@ -0,0 +1,23 @@
+// Global scope and functions
+
+/* Use of the var keyword when creating a variable is critical. If a variable
+is created without it, it is seen as a global variable. */
+// Declare your variable here
+var myGlobal = 10
+
+function fun1() {
+  // Assign 5 to oopsGlobal Here
+  oopsGlobal = 5;
+}
+
+// Only change code above this line
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
