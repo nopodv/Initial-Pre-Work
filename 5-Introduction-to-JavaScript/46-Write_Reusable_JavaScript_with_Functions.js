a3114e079ed5536725943a80744b69f27a83f2aa
nopoabb <huntergreen_2k@yahoo.com>
2019-05-19 20:31:22 -0400

    Added function and comments about what I Iearned.

---
+++ 5-Introduction-to-JavaScript/46-Write_Reusable_JavaScript_with_Functions.js
@@ -0,0 +1,16 @@
+// Write reusable javascript
+
+/* Uses a function to call a variable and prints it in the console. Calling
+the function after it is defined. */
+// Example
+function ourReusableFunction() {
+  console.log("Heyya, World");
+}
+
+ourReusableFunction();
+
+// Only change code below this line
+function reusableFunction() {
+    console.log("Hi World");
+}
+reusableFunction();
