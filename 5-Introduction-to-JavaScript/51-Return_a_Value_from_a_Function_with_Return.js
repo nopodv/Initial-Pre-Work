837ea3ab01efe96c4b5f3850470225b8b9624636
nopoabb <huntergreen_2k@yahoo.com>
2019-05-19 21:46:00 -0400

    Added return to get value and comments about what I Iearned.

---
+++ 5-Introduction-to-JavaScript/51-Return_a_Value_from_a_Function_with_Return.js
@@ -0,0 +1,15 @@
+// Return a value from a function
+
+// Uses return to get a value from a function.
+// Example
+function minusSeven(num) {
+  return num - 7;
+}
+
+// Only change code below this line
+function timesFive(num) {
+return num * 5;
+}
+
+
+console.log(minusSeven(10));
