ca97c30056d135d1d782b0488a6d08ff9cddb975
nopoabb <huntergreen_2k@yahoo.com>
2019-05-19 22:22:55 -0400

    Added variable from returned function and comments about what I Iearned.

---
+++ 5-Introduction-to-JavaScript/53-Assignment_with_a_Returned_Value.js
@@ -0,0 +1,21 @@
+// Assignment with a returned value
+
+// Assigns the value of a function that was returned to new a variable.
+// Example
+var changed = 0;
+
+function change(num) {
+  return (num + 5) / 3;
+}
+
+changed = change(10);
+
+// Setup
+var processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+// Only change code below this line
+processed = processArg(7);
