d84b7de81d17310754dac92fff26ca3c4e58542b
nopoabb <huntergreen_2k@yahoo.com>
2019-05-21 13:28:55 -0400

    Added boolean to function and comments about what I Iearned.

---
+++ 5-Introduction-to-JavaScript/77-Returning_Boolean_Values_from_Functions.js
@@ -0,0 +1,11 @@
+// Return boolean values from functions
+
+// Gives true or false when calling a function. Reduces code to input.
+function isLess(a, b) {
+  // Fix this code
+  return a < b;
+
+}
+
+// Change these values to test
+isLess(15, 10);
