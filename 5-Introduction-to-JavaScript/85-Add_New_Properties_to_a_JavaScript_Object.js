aefbaafd8310e39eb6aaceb29f9e61e1a86ceff6
nopoabb <huntergreen_2k@yahoo.com>
2019-05-21 16:42:43 -0400

    Added new property and value and comments about what I Iearned.

---
+++ 5-Introduction-to-JavaScript/85-Add_New_Properties_to_a_JavaScript_Object.js
@@ -0,0 +1,23 @@
+// Add new property to a object
+
+// Adds property with dot or bracket notation.
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+ourDog.bark = "bow-wow";
+
+// Setup
+var myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line.
+myDog["bark"] = "growl;"
