029e53c3e02e88a66caf9794d91b9c52cb2aa405
nopoabb <huntergreen_2k@yahoo.com>
2019-05-21 18:33:38 -0400

    Added properties to object and comments about what I Iearned.

---
+++ 5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
@@ -0,0 +1,30 @@
+// Manipulate complex objects
+
+// Adds properties for a new record into the object.
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+
+  {
+    "artist": "Little Dragon",
+    "title": "Nabuma Rubberband",
+    "release_year": 2014,
+    "formats": [
+      "CD",
+      "Streaming",
+      "LP"
+    ]
+
+  }
+
+    // Add record here
+];
