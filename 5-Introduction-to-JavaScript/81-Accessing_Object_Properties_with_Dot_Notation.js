5d1a3edfd56a1d9592a48634c09c5d3bc484af66
nopoabb <huntergreen_2k@yahoo.com>
2019-05-21 15:54:15 -0400

    Added dot notation comments about what I Iearned.

---
+++ 5-Introduction-to-JavaScript/81-Accessing_Object_Properties_with_Dot_Notation.js
@@ -0,0 +1,15 @@
+// Accessing object properties with dot notation
+
+/* Using the dot notation, this appends an objects property name after then
+object in order to store it in a new variable. */
+// Setup
+var testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Only change code below this line
+
+var hatValue = testObj.hat;      // Change this line
+var shirtValue = testObj.shirt;    // Change this line
