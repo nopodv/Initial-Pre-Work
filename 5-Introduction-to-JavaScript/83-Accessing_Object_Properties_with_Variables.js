92acfd8f8a0a400c6043098cefca6e7ee5fea439
nopoabb <huntergreen_2k@yahoo.com>
2019-05-21 16:32:57 -0400

    Added and stores values to variables comments about what I Iearned.

---
+++ 5-Introduction-to-JavaScript/83-Accessing_Object_Properties_with_Variables.js
@@ -0,0 +1,15 @@
+// Accessing object properties with variables
+
+/* Stores property names in a new variable. Then creates a new variable to
+call the one created. */
+// Setup
+var testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line;
+
+var playerNumber = 16;       // Change this Line
+var player = testObj[playerNumber];   // Change this Line
