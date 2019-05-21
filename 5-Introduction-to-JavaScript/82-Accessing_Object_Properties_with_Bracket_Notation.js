423309056b5e91c8713e4610cdc7aa355b6f84d1
nopoabb <huntergreen_2k@yahoo.com>
2019-05-21 16:05:36 -0400

    Added bracket notation comments about what I Iearned.

---
+++ 5-Introduction-to-JavaScript/82-Accessing_Object_Properties_with_Bracket_Notation.js
@@ -0,0 +1,16 @@
+// Accessing object properties with bracket notation
+
+/* Using the bracket notation, this appends an objects property name after the
+object in order to store it in a new variable. However brackets are used due
+to a property name having more than one word with a space. */
+// Setup
+var testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Only change code below this line
+
+var entreeValue = testObj["an entree"];   // Change this line
+var drinkValue = testObj["the drink"];    // Change this line
