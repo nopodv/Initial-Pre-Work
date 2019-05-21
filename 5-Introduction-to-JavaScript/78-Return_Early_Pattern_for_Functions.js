58cef70f8df60f4e98f0b8cb11cf0a852b43164b
nopoabb <huntergreen_2k@yahoo.com>
2019-05-21 13:39:00 -0400

    Added values to function and comments about what I Iearned.

---
+++ 5-Introduction-to-JavaScript/78-Return_Early_Pattern_for_Functions.js
@@ -0,0 +1,17 @@
+// Return early pattern for functions
+
+// Adds if statement and values to allow early return.
+// Setup
+function abTest(a, b) {
+  // Only change code below this line
+  if (a < 0 || b < 0)
+  return undefined;
+
+
+  // Only change code above this line
+
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+
+// Change values below to test your code
+abTest(2,2);
