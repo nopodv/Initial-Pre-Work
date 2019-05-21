1336a88fec4a0e34a49955e09cdf215ad2a22a7d
nopoabb <huntergreen_2k@yahoo.com>
2019-05-21 15:44:06 -0400

    Added properties to object and comments about what I Iearned.

---
+++ 5-Introduction-to-JavaScript/80-Build_JavaScript_Objects.js
@@ -0,0 +1,24 @@
+// Build javascript objects
+
+/* Adds properties to build an object. Combines numbers and strings to show
+the variety of types that can be created. In the property names and the
+data as well. */
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  "name": "Fido",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["Barney", "Jeff"]
+
+
+
+};
