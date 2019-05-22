7b27e720ec3dc5973ebcc4ae983727244d2cea44
nopoabb <huntergreen_2k@yahoo.com>
2019-05-22 17:56:13 -0400

    Added while loop and comments about what I Iearned.

---
+++ 5-Introduction-to-JavaScript/93-Iterate_with_JavaScript_While_Loops.js
@@ -0,0 +1,13 @@
+// Iterate with while loops
+
+/* This while loop can run continuously until it reaches a point that it has
+finished processing all the statments. */
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+var i = 0;
+while (i < 5) {
+  myArray.push(i);
+  i++;
+}
