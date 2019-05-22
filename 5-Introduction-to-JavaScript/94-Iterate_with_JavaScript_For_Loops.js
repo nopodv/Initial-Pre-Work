a73a7eec0a09733dac13eba1f9885dde608ce0ac
nopoabb <huntergreen_2k@yahoo.com>
2019-05-22 18:06:15 -0400

    Added for loop and comments about what I Iearned.

---
+++ 5-Introduction-to-JavaScript/94-Iterate_with_JavaScript_For_Loops.js
@@ -0,0 +1,18 @@
+// Iterate with for loops
+
+/* Creates a loop to to add a number to an array "for" a specified number of
+times. Then it will stop. */
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 5; i++) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for (var i = 1; i < 6; i++) {
+  myArray.push(i);
+}
