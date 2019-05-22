be768ee9d01ecae4073e44fe88e1080c4acd357c
nopoabb <huntergreen_2k@yahoo.com>
2019-05-22 18:16:06 -0400

    Added for loop w/odd numners and comments about what I Iearned.

---
+++ 5-Introduction-to-JavaScript/95-Iterate_Odd_Numbers_With_a_For_Loop.js
@@ -0,0 +1,20 @@
+// Iterate odd numbers with a for loop
+
+/* Creates a loop to to add odd numbers to an array "for" a specified number of
+times. Then it will stop. Being sure to change the final expression is what
+will allow the result to increment how you like. */
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 10; i += 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+
+for (var i = 1; i < 10; i += 2) {
+  myArray.push(i);
+}
