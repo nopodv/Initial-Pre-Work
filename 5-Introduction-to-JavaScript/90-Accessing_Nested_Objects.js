e187e69ce39dab2710bae01ae593efd3e05258e8
nopoabb <huntergreen_2k@yahoo.com>
2019-05-22 15:44:29 -0400

    Added info to value for accessing nested property and comments about what I Iearned.

---
+++ 5-Introduction-to-JavaScript/90-Accessing_Nested_Objects.js
@@ -0,0 +1,17 @@
+// Access nested objects
+
+// Gets nested items that are several levels deep in a nested property.
+// Setup
+var myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+var gloveBoxContents = myStorage.car.inside["glove box"]; // Change this line
