76fac528af250d93734f7d1b70797b876210a466
nopoabb <huntergreen_2k@yahoo.com>
2019-05-22 18:26:09 -0400

    Added for loop w/odd numbers getting smaller and comments about what I Iearned.

--- 5-Introduction-to-JavaScript/96-Count_Backwards_With_a_For_Loop.js
+++ 5-Introduction-to-JavaScript/96-Count_Backwards_With_a_For_Loop.js
@@ -1,1 +1,19 @@
+// Count backwards with a for loop
 
+/* Works the same as a for loop going forward (adding positive iterations)
+by changing the initial, condition and final expression. */
+// Example
+var ourArray = [];
+
+for (var i = 10; i > 0; i -= 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+
+for (var i = 9; i > 0; i -=2) {
+  myArray.push(i);
+}
