464c74b28c6bf1c045d229bb0db11d71c82f62fc
nopoabb <huntergreen_2k@yahoo.com>
2019-05-23 15:22:29 -0400

    Added function for random number and comments about what I Iearned.

---
+++ 5-Introduction-to-JavaScript/101-Generate_Random_Fractions_with_JavaScript.js
@@ -0,0 +1,11 @@
+// Generate random fractions
+
+// Adds function to give a random number.
+function randomFraction() {
+
+  // Only change code below this line.
+Math.random();
+  return Math.random();
+
+  // Only change code above this line.
+}
