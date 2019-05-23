2b0d933b74d38fe9579786b419eff0c947bf5a55
nopoabb <huntergreen_2k@yahoo.com>
2019-05-23 16:52:26 -0400

    Added conditional operator to function and comments about what I Iearned.

---
+++ 5-Introduction-to-JavaScript/106-Use_the_Conditional_(Ternary)_Operator.js
@@ -0,0 +1,9 @@
+// Use the conditional operator
+
+/* Creates and if else statement in one line. It streamlines the function
+more or less. */
+function checkEqual(a, b) {
+  return a == b ? true : false;
+}
+
+checkEqual(1, 2);
