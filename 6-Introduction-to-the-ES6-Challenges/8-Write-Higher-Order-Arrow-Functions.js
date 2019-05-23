642ae1a9f1e262243577b9609d5e8423e022fbaf
nopoabb <huntergreen_2k@yahoo.com>
2019-05-23 19:06:08 -0400

    Added arrow function for multiple function chains and comments about what I Iearned.

---
+++ 6-Introduction-to-the-ES6-Challenges/8-Write-Higher-Order-Arrow-Functions.js
@@ -0,0 +1,14 @@
+// Write higher order arrow functions
+
+/* Using arrow functions to combine more functions to make a result faster
+and concise. */
+const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
+const squareList = (arr) => {
+  "use strict";
+  // change code below this line
+      const squaredIntegers = arr.filter( (num) => num > 0 && num % parseInt(num) === 0 ).map( (num) => Math.pow(num, 2) );  // change code above this line
+  return squaredIntegers;
+};
+// test your code
+const squaredIntegers = squareList(realNumberArray);
+console.log(squaredIntegers);
