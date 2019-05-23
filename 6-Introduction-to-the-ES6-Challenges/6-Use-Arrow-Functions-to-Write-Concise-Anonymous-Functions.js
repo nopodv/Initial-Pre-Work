ce0ccf5140cb8e46f65b5a3e15f13b1234f7695a
nopoabb <huntergreen_2k@yahoo.com>
2019-05-23 18:30:55 -0400

    Added arrow function and comments about what I Iearned.

---
+++ 6-Introduction-to-the-ES6-Challenges/6-Use-Arrow-Functions-to-Write-Concise-Anonymous-Functions.js
@@ -0,0 +1,5 @@
+// Use arrow function to write concise anonymous function
+
+/* Makes the use of arrow functions to streamline the output to fewer lines
+and make it easier to read. */
+const magic = () => new Date()
