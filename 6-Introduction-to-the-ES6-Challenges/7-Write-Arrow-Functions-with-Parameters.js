77861f934b7696bd799cff796b552b1693fc6c4d
nopoabb <huntergreen_2k@yahoo.com>
2019-05-23 18:37:31 -0400

    Added concatenation using arrow function and comments about what I Iearned.

---
+++ 6-Introduction-to-the-ES6-Challenges/7-Write-Arrow-Functions-with-Parameters.js
@@ -0,0 +1,7 @@
+// Write arrow functions with parameters
+
+// Uses concatenation parameter in arrow function to combine items together.
+const myConcat = (arr1, arr2) => arr1.concat(arr2);
+
+// test your code
+console.log(myConcat([1, 2], [3, 4, 5]));
