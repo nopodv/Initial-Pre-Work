d6c56c26ff9ee2609b4b0a6556c750a39d368b7f
nopoabb <huntergreen_2k@yahoo.com>
2019-05-23 15:47:43 -0400

    Added function for random whole numbers and comments about what I Iearned.

---
+++ 5-Introduction-to-JavaScript/102-Generate_Random_Whole_Numbers_with_JavaScript.js
@@ -0,0 +1,11 @@
+// Generate random whole numbers
+
+// Adds function to create random numbers that are whole...so not decimals.
+var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+
+function randomWholeNum() {
+
+  // Only change code below this line.
+Math.floor(Math.random() * 10);
+  return Math.floor(Math.random() * 10);
+}
