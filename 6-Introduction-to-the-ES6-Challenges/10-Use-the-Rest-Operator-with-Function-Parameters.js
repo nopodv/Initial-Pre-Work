795723890396cadd2bf82fb8f69f2820f89f1f6e
nopoabb <huntergreen_2k@yahoo.com>
2019-05-23 20:04:32 -0400

    Added rest operator and comments about what I Iearned.

---
+++ 6-Introduction-to-the-ES6-Challenges/10-Use-the-Rest-Operator-with-Function-Parameters.js
@@ -0,0 +1,10 @@
+// Use the rest operator with function parameters
+
+// Adds rest operator to function to reduce function lines.
+const sum = (function() {
+  "use strict";
+  return function sum(...args) {
+    return args.reduce((a, b) => a + b, 0);
+  };
+})();
+console.log(sum(1, 2, 3)); // 6
