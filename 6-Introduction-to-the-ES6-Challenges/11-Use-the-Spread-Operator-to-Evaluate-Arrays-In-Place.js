b04de3b7e0c3fa8ec669efa8d105153b646ab8c3
nopoabb <huntergreen_2k@yahoo.com>
2019-05-23 20:12:38 -0400

    Added spread operator and comments about what I Iearned.

---
+++ 6-Introduction-to-the-ES6-Challenges/11-Use-the-Spread-Operator-to-Evaluate-Arrays-In-Place.js
@@ -0,0 +1,10 @@
+// Use the spread operator to evaluate arrays in place
+
+// Adds array into another array.
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+(function() {
+  "use strict";
+  arr2 = [...arr1]; // change this line
+})();
+console.log(arr2);
