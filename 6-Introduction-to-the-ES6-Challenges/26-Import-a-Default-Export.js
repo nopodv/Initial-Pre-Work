3d2748b32c84c04f76ba59e242e328435c840826
nopoabb <huntergreen_2k@yahoo.com>
2019-05-25 17:24:38 -0400

    Added import default export and comments about what I Iearned.

---
+++ 6-Introduction-to-the-ES6-Challenges/26-Import-a-Default-Export.js
@@ -0,0 +1,7 @@
+// Import a default export
+
+/* Imports the default file from the same directory using default import.
+This does not utilize the brackets like normal import. */
+"use strict";
+subtract(7,4);
+import subtract from "math_functions";
