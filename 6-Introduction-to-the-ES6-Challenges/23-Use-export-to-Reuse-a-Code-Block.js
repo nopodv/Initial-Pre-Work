fde71ff5ee61e72e41762c5570acc4a7335fc937
nopoabb <huntergreen_2k@yahoo.com>
2019-05-25 17:03:37 -0400

    Added export for variables and comments about what I Iearned.

---
+++ 6-Introduction-to-the-ES6-Challenges/23-Use-export-to-Reuse-a-Code-Block.js
@@ -0,0 +1,7 @@
+// Use export to reuse a code block
+
+// Exports variables. The benefit is to use the variables in another program.
+"use strict";
+
+export const foo = "bar";
+export const bar = "foo";
