7bfed3006f71af3208d8f287236303e5f1b5654a
nopoabb <huntergreen_2k@yahoo.com>
2019-05-25 16:53:30 -0400

    Added import to retrieve function and comments about what I Iearned.

---
+++ 6-Introduction-to-the-ES6-Challenges/22-Understand-the-Differences-Between-import-and-require.js
@@ -0,0 +1,7 @@
+// Undertand the differences between important and require
+
+/* Imports only selective info from an external source as opposed to the whole
+of the files themselves. This is helpful when needing only a small section of
+the data. */
+"use strict";
+import { capitalizeString } from "string_functions"
