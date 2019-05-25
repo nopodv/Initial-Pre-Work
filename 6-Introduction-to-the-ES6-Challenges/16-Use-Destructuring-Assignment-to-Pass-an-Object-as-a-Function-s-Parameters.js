90441dff425ebf19c5349f42127e91462718ed68
nopoabb <huntergreen_2k@yahoo.com>
2019-05-25 14:23:23 -0400

    Added desctructured assignment inside function comments about what I Iearned.

---
+++ 6-Introduction-to-the-ES6-Challenges/16-Use-Destructuring-Assignment-to-Pass-an-Object-as-a-Function-s-Parameters.js
@@ -0,0 +1,25 @@
+// Use destructuring to pass an object as a functions parameter
+
+/* Compresses the needed info asked for in the function itself. This allows
+for a more efficient, cleaner and more legible code as a benefit. */
+const stats = {
+  max: 56.78,
+  standard_deviation: 4.34,
+  median: 34.54,
+  mode: 23.87,
+  min: -0.75,
+  average: 35.85
+};
+const half = (function() {
+  "use strict"; // do not change this line
+
+  // change code below this line
+  return function half({ max, min }) {
+    // use function argument destructuring
+    return (stats.max + stats.min) / 2.0;
+  };
+  // change code above this line
+
+})();
+console.log(stats); // should be object
+console.log(half(stats)); // should be 28.015
