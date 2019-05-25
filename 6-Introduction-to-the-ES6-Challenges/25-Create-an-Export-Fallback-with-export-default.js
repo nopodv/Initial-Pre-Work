0061bc53c60a29b9f5e74d22ca387c9a047a9de1
nopoabb <huntergreen_2k@yahoo.com>
2019-05-25 17:17:07 -0400

    Added export fallback with default and comments about what I Iearned.

---
+++ 6-Introduction-to-the-ES6-Challenges/25-Create-an-Export-Fallback-with-export-default.js
@@ -0,0 +1,5 @@
+// Create an export fallback with export default
+
+// Creates a function to be exported that is the template, or default.
+"use strict";
+export default function subtract(x,y) {return x - y;}
